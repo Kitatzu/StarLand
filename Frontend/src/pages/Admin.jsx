0527c1d1ab77faf3e7cc74b4c121556e871e22cb
Exequiel Rodriguez <exe922@gmail.com>
2024-03-15 20:13:32 -0300

    Table CRUD Admin

--- Frontend/src/pages/Admin.jsx
+++ Frontend/src/pages/Admin.jsx
@@ -1,7 +1,44 @@
+import css from "../css/Admin.module.css";
+import { useProductStore } from "../store/Store";
+
 export const Admin = () => {
+  const { products } = useProductStore();
+
   return (
-    <div>
-      <h1> Administracion </h1>
+    <div className={css.admin_container}>
+      <button className={css.create_btn}>Crear Nuevo Producto</button>
+      <div className={css.table_container}>
+        <table className={css.table}>
+          <thead>
+            <tr>
+              <th>Nombre</th>
+              <th>Precio</th>
+              <th>Descripción</th>
+              <th> Acciones </th>
+              {/* Agrega más encabezados si es necesario */}
+            </tr>
+          </thead>
+          <tbody>
+            {products.map((product) => (
+              <tr key={product.id}>
+                <td>{product.name}</td>
+                <td>${product.price}</td>
+                <td>{product.description}</td>
+                <td>
+                  <div className={css.btn_container}>
+                    <button onClick={() => handleEdit(product.id)}>
+                      Editar
+                    </button>
+                    <button onClick={() => handleDelete(product.id)}>
+                      Eliminar
+                    </button>
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
